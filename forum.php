43db04c4915170af600a03773dc79c0e0f41c7db
m-moriarty <44737225+m-moriarty@users.noreply.github.com>
2018-11-12 12:04:28 -0500

    update forum.php
    replacing post.php for file restructuring/renaming

--- forum.php
+++ forum.php
@@ -1,5 +1,32 @@
+<?php
+session_start();
+ include_once 'header.php';
 
+?>
+<section class="main-container">
+  <div class="main-wrap">
+    <h2>Post</h2>
+    <form class="Post-form" action="includes/post.inc.php"method="POST">
+      <input type="text" name="context" placeholder="Words...">
+      <button type="submit" name="submit">Post</button>
+    </form>
+    <?php
+    //display 30 most recent posts.
+    include 'includes/dbm.inc.php';
+    if(mysqli_connect_errno()){echo "Failed to connect to database";}
+    $sql="SELECT p_id, p_user, p_context FROM posts ORDER BY p_id";
+    $result = mysqli_query($conn, $sql);
+    $rows = mysqli_num_rows($result);
+    if($rows < 1){
+      echo 'sorry no dice';
+    }
+    else{
+      echo 'array initated';
+    }
 
-  <?php
-            echo 'Display of historical posts that all have a sub thread to comment in.';
-  ?>
+    ?>
+  </div>
+</section>
+<?php
+  include_once 'footer.php';
+?>
