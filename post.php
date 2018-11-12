8120f85fcc940d83aecc7e1f5b77ce220ed03104
m-moriarty <44737225+m-moriarty@users.noreply.github.com>
2018-11-12 11:06:45 -0500

    update post.php
    added initial setup to call post history

--- post.php
+++ post.php
@@ -10,7 +10,20 @@ session_start();
       <input type="text" name="context" placeholder="Words...">
       <button type="submit" name="submit">Post</button>
     </form>
-
+    <?php
+    //display 30 most recent posts.
+    include 'includes/dbm.inc.php';
+    if(mysqli_connect_errno()){echo "Failed to connect to database";}
+    $sql="SELECT p_id, p_user, p_context FROM posts ORDER BY p_id";
+    $result = mysqli_query($conn, $sql);
+    $rows = mysqli_num_rows($result);
+    if($rows < 1){
+      echo 'sorry no dice';
+    }
+    else{
+      echo 'array initated';
+    }
+    ?>
   </div>
 </section>
 <?php
